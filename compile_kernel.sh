ae697634b5fecd16dba985e1a7de2124e379d7b3
sharongoldi <sharon19g@gmail.com>
2019-05-04 19:46:31 +0300

    1. added unnecessary files to git-ignore. 2. added compile_kernel.sh script.

---
+++ compile_kernel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#find files modified in the past 24 hours, ingnore hidden files (get only .c files)
+files=`find . -not -path "*/\.*" -name "*.c" -mtime -1 -print`
+
+#for every file copy to the correct place in the kernel library
+for file in $files; do
+        cp $file "/usr/src/linux-2.4.18-14custom${file:1}"      
+done
+
+cd /usr/src/linux-2.4.18-14custom
+make bzImage
+cd /mnt/hgfs/shared_folder/os234123
+cp /usr/src/linux-2.4.18-14custom/arch/i386/boot/bzImage /boot/vmlinuz-2.4.18-14custom
